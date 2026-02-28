b88268fab0f68728419750f342d1516ff5e98727
muk4de <moritashouta0@gmail.com>
2026-02-28 16:25:42 +0900

    Add Runner Script

---
+++ Assets/WorkSpace/Shota/Scripts/RunnerInputHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class RunnerInputHandler : MonoBehaviour
+{
+    CharacterController _controller;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
