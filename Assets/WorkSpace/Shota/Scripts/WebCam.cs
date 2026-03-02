20a822a294e5c1442e628be7e8a2a6756205bf1b
muk4de <moritashouta0@gmail.com>
2026-03-02 23:58:13 +0900

    Web Camera Test

---
+++ Assets/WorkSpace/Shota/Scripts/WebCam.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.WebCam;
+
+// Webカメラ
+public class WebCam : MonoBehaviour
+{
+    private static int INPUT_SIZE = 256;
+    private static int FPS = 30;
+
+    // UI
+    RawImage rawImage;
+    WebCamTexture webCamTexture;
+
+    // スタート時に呼ばれる
+    void Start()
+    {
+        CameraInit();
+    }
+
+    public void CameraInit()
+    {
+        if (webCamTexture == null)
+        {
+            Debug.Log("Webカメラが見つかりません");
+            return;
+        }
+        // Webカメラの開始
+        this.rawImage = GetComponent<RawImage>();
+        this.webCamTexture = new WebCamTexture(INPUT_SIZE, INPUT_SIZE, FPS);
+        this.rawImage.texture = this.webCamTexture;
+        this.webCamTexture.Play();
+    }
+}
